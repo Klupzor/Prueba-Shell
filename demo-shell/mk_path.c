9279fc941776c3d0f847bbc216e1f6c8e450b219
klupzor <lopezgand@gmail.com>
2019-04-10 23:08:02 +0000

    betty and the arguments

--- demo-shell/mk_path.c
+++ demo-shell/mk_path.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 /**
- * _strcpy - prints reverse  copy.
- *
- * @s: char string.
- * @a: copy char string.
+ * mk_path - .
+ * @command: is the command to be organized.
+ * @path: is the direction to be organized.
  * Return: Always 0.
  */
+
 char **mk_path(char **command, char *path)
 {
 	int size_cmd, size_path, stm;
@@ -13,9 +13,8 @@ char **mk_path(char **command, char *path)
 
 	stm = 1024;
 	cp = malloc(sizeof(char) * stm);
-	if(!cp)
-		return(0);
-	printf("command: %s path: %s\n", command[0], path);
+	if (!cp)
+		return (0);
 	for (size_path = 0; path[size_path] != '\0'; size_path++)
 	{
 		cp[size_path] = path[size_path];
@@ -23,9 +22,6 @@ char **mk_path(char **command, char *path)
 	cp[size_path] = '/';
 	size_path++;
 	for (size_cmd = 0 ; command[0][size_cmd] != '\0' ; size_cmd++)
-	{
-		//printf("cp: %s\n", cp);
-		cp[size_cmd + size_path] = command[0][size_cmd];
 	}
 	add_argv(command, cp, 0);
 	free(cp);

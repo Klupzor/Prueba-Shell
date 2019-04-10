64cb5847dd268a029f977ff1cd3c2a0cb32a7fc5
klupzor <lopezgand@gmail.com>
2019-04-10 14:59:44 +0000

    se arreglo!

--- motherBox.c
+++ motherBox.c
@@ -37,13 +37,14 @@ void free_motherBox(char **array, int size)
 }
 int addBox(char **motherBox, char *str, int pos)
 {
+	free(motherBox[pos]);
 	motherBox[pos] = _strdup(str);
 	return (0);
 }
 
 int main()
 {
-	char input[]="ls a1 a2\n", *dest[100];
+	char input[]="ls -l -a       s\n", *dest[1000];
 	char **motherBox, *token;
 	int i, cont, count, size;
 
@@ -52,24 +53,27 @@ int main()
 	token = strtok(input, " ");
         for (count = 0; token != NULL; count++)
         {
-                dest[count] = token;
-                printf("args: %s\n", token);
+		dest[count] = token;
+		printf("args: %s\n", token);
                 token = strtok(NULL, " ");
         }
+	dest[count] = '\0';
+	count++;
 	printf("num args: %i\n", count);
 	size = count;
-	motherBox = (char **) malloc(sizeof(char) * (size));
+	motherBox = (char **) malloc(sizeof(char *) * (size));
 	if (motherBox == 0)
 		return (-1);
 
 	for(cont = 0 ; cont < size ; cont ++)
 	{
 	motherBox[cont] = _strdup(dest[cont]);
+		
 	//addBox(motherBox,dest[cont] , cont);
 	//addBox(motherBox, "segundo", 1);
-	//addBox(motherBox, "modificado", 0);
 	//addBox(motherBox, "tercero", 2);
 	}
+	//addBox(motherBox, "modificado", 0);
 	for (i = 0 ; i < size ; i++)
 	{
 		printf("on mother: %s\n", motherBox[i]);

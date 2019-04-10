f149bd5e9060e58ee2b74115fea9d203961fc21d
klupzor <lopezgand@gmail.com>
2019-04-10 02:54:32 +0000

    creando funcion para guardar args, pero todavia genera problemas valgrind

---
+++ motherBox.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *_strdup(char *str)
+{
+        char *array;
+        int i, size;
+
+	if (!str)
+                return (0);
+        for (size = 1; str[size] != '\0'; size++)
+        {
+        }
+        array =  malloc((size * sizeof(char)) + 1);
+        if (array == NULL)
+                return (NULL);
+        for (i = 0; str[i] != '\0'; i++)
+        {
+                array[i] = str[i];
+        }
+        array[i] = '\0';
+        return (array);
+}
+void free_motherBox(char **array, int size)
+{
+	int cont;
+
+	if (!(array == NULL || size == 0))
+	{
+		for (cont = 0; cont < size ; cont++)
+		{
+			free(array[cont]);
+		}
+		free(array);
+	}
+}
+int addBox(char **motherBox, char *str, int pos)
+{
+	motherBox[pos] = _strdup(str);
+	return (0);
+}
+
+int main()
+{
+	char input[]="ls a1 a2\n", *dest[100];
+	char **motherBox, *token;
+	int i, cont, count, size;
+
+	
+	strtok(input, "\n");
+	token = strtok(input, " ");
+        for (count = 0; token != NULL; count++)
+        {
+                dest[count] = token;
+                printf("args: %s\n", token);
+                token = strtok(NULL, " ");
+        }
+	printf("num args: %i\n", count);
+	size = count;
+	motherBox = (char **) malloc(sizeof(char) * (size));
+	if (motherBox == 0)
+		return (-1);
+
+	for(cont = 0 ; cont < size ; cont ++)
+	{
+	motherBox[cont] = _strdup(dest[cont]);
+	//addBox(motherBox,dest[cont] , cont);
+	//addBox(motherBox, "segundo", 1);
+	//addBox(motherBox, "modificado", 0);
+	//addBox(motherBox, "tercero", 2);
+	}
+	for (i = 0 ; i < size ; i++)
+	{
+		printf("on mother: %s\n", motherBox[i]);
+	}
+	free_motherBox(motherBox, size);
+	return (0);
+}
---
+++ pruebaval.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create a dynamic array.
+ * @size: size of array.
+ * @c: char to fill array
+ *
+ * Return: ponter array or null.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return (0);
+	array = malloc(size * sizeof(char));
+	if (array == 0)
+		return (0);
+	for (i = 0 ; i < size ; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}
+
+int main()
+{
+	char *cual;
+
+	cual = create_array(10, 'a');
+	cual[9] = '\0';
+	printf("%s\n", cual);
+
+	free(cual);
+
+}
